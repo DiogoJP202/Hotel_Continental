048252f2cfc3c4d86dc21a16f1e0192ec99149a9
diogojp202@gmail.com <diogojp202@gmail.com>
2024-06-21 07:54:45 -0300

    script abastecer carro finalizado e página ar condicionado inicializada

---
+++ assets/js/arCondicionado.js
@@ -0,0 +1,4 @@
+const empresaNome = document.querySelector("#IEmpresaNome").value;
+const valorAparelho = document.querySelector("#IValorAparelho").value;
+const porcentagemDesconto = document.querySelector("#IPorcentagemDesconto").value;
+const quantidadeAparelhos = document.querySelector("#IQuantidadeAparelhos").value;
--- assets/js/verificarAbastecimento.js
+++ assets/js/verificarAbastecimento.js
@@ -5,32 +5,28 @@ const form = document.querySelector("form");
 form.addEventListener("submit", e => {
     e.preventDefault();
 
-    const alcoolW = document.querySelector("#IWayneOilA").value;
-    const gasolinaW = document.querySelector("#IWayneOilG").value;
-    const alcoolS = document.querySelector("#IStarkPetrolA").value;
-    const gasolinaS = document.querySelector("#IStarkPetrolG").value;
+    const alcoolW = Number(document.querySelector("#IWayneOilA").value);
+    const gasolinaW = Number(document.querySelector("#IWayneOilG").value);
+    const alcoolS = Number(document.querySelector("#IStarkPetrolA").value);
+    const gasolinaS = Number(document.querySelector("#IStarkPetrolG").value);
     
     if(alcoolW == alcoolS && gasolinaS == gasolinaW && alcoolS == gasolinaW) return alert("Os valores são iguais, é mais barato abaster com álcool.");
 
-    if((alcoolS > alcoolW && alcoolS > gasolinaS && alcoolS > gasolinaW)){
-        return alert(`${window.name}, é mais barato abastecer com álcool no posto Wayne Oil.`);
-    }
+    if(alcoolS > (gasolinaW - (gasolinaW * 0.30)) || alcoolW > (gasolinaW - (gasolinaW * 0.30)) || alcoolS > (gasolinaS - (gasolinaS * 0.30)) || alcoolW > (gasolinaS - (gasolinaS * 0.30))) if(gasolinaW < gasolinaS){
+            return alert(`${window.name}, é mais barato abastecer com gasolina no posto Wayne Oil.`);
+        } else {
+            return alert(`${window.name}, é mais barato abastecer com gasolina no posto Stark Petrol.`);
+        }
 
-    if(alcoolW > alcoolS && alcoolW > gasolinaS && alcoolW > gasolinaW){
-        return alert(`${window.name}, é mais barato abastecer com gasolina no posto Wayne Oil.`);
-    }
+    if((alcoolS > alcoolW && alcoolS > gasolinaS && alcoolS > gasolinaW)) return alert(`${window.name}, é mais barato abastecer com álcool no posto Wayne Oil.`);
 
-    if(gasolinaS > alcoolW && gasolinaS > alcoolS && gasolinaS > gasolinaW){
-        return alert(`${window.name}, é mais barato abastecer com álcool no posto Stark Petrol.`);
-    }
+    if(alcoolW > alcoolS && alcoolW > gasolinaS && alcoolW > gasolinaW) return alert(`${window.name}, é mais barato abastecer com gasolina no posto Wayne Oil.`);
+
+    if(gasolinaS > alcoolW && gasolinaS > alcoolS && gasolinaS > gasolinaW) return alert(`${window.name}, é mais barato abastecer com álcool no posto Stark Petrol.`);
     
-    if(gasolinaW > alcoolW && gasolinaW > gasolinaS && gasolinaW > alcoolS){
-        return alert(`${window.name}, é mais barato abastecer com gasolina no posto Stark Petrol.`);
-    }
+    if(gasolinaW > alcoolW && gasolinaW > gasolinaS && gasolinaW > alcoolS) return alert(`${window.name}, é mais barato abastecer com gasolina no posto Stark Petrol.`);
 
-    if(gasolinaW == gasolinaS){
-        return alert("Valores da gasolina são iguais, opte por qualquer uma das duas");
-    }
+    if(gasolinaW == gasolinaS) return alert("Valores da gasolina são iguais, opte por qualquer uma das duas");
 
     return alert(`${window.name}, é mais barato abastecer com gasolina no posto Wayne Oil.`);
 });
